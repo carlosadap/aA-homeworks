8e6d7c6c4b14f008d5518e8e9c051b012e457769
Carlos Pinheiro <carlosadap@gmail.com>
2021-02-17 10:43:47 +0100

    Add the children to its parent children

--- ruby/data_structures/poly_tree_node/skeleton/lib/00_tree_node.rb
+++ ruby/data_structures/poly_tree_node/skeleton/lib/00_tree_node.rb
@@ -1,10 +1,15 @@
 class PolyTreeNode
-  attr_accessor :parent
-  attr_reader :children, :value
+  
+  attr_reader :children, :value, :parent
 
   def initialize(value)
     @value = value
     @parent = nil
     @children = []
+  end
+
+  def parent=(new_parent)
+    @parent = new_parent
+    self.parent.children << self unless self.parent.nil?
   end
 end
