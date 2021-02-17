e9c5c2cf2480af13095ad1f3162f64277c20c5b9
Carlos Pinheiro <carlosadap@gmail.com>
2021-02-17 08:46:49 +0100

    Create Queue class

---
+++ ruby/data_structures/DIY_ADTs/queue.rb
@@ -0,0 +1,17 @@
+class Queue
+  def initialize
+    @ivar = []
+  end
+
+  def enqueue(el)
+    @ivar.unshift(el)
+  end
+
+  def dequeue
+    @ivar.pop
+  end
+
+  def peek
+    @ivar[-1]
+  end
+end
